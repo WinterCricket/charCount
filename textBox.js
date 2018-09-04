39b7c2172e8b8771017267a8235f3b6af2c90c09
WinterCricket <hodhodhod2003@gmail.com>
2018-09-04 13:28:33 -0500

    initial commit

---
+++ textBox.js
@@ -0,0 +1,26 @@
+document.getElementById("whiteBoard").onclick=function() {
+	var canvas = document.getElementById("whiteBoard");
+	var context = canvas.getContext("2d");
+
+	context.lineWidth = 30;
+	context.strokeStyle = "blue";
+
+	//draw line with standard butt ending
+
+	context.moveTo(25, 50); //horizontal, vertical
+	context.lineTo(400, 150);
+	context.lineCap = "butt";
+	context.stroke();
+
+
+	//draw second line change color etc
+
+	context.lineWidth = 20;
+	context.strokeStyle = "red";
+
+	context.beginPath();
+	context.moveTo(25, 120);
+	context.lineTo(400, 120);
+	context.lineCap = "butt";
+	context.stroke();
+}
