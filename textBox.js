165fa7dc8a25d0dbe88bc92c751891ee8243d39a
WinterCricket <hodhodhod2003@gmail.com>
2018-09-05 20:38:25 -0500

    try flex box and a form

--- textBox.js
+++ textBox.js
@@ -1,21 +1,32 @@
 
 
-document.getElementById("words").onclick=function() {
-	var canvas = document.getElementById("words");
-	var context = canvas.getContext("2d");
+//  document.getElementById("words").onclick=function() {
+//  	var canvas = document.getElementById("words");
+// 	var context = canvas.getContext("2d");
 
-	context.lineWidth = 30;
-	context.strokeStyle = "blue";
+//  	context.lineWidth = 30;
+//  	context.strokeStyle = "blue";
 
-	//draw line with standard butt ending
+// // 	//draw line with standard butt ending
 
-	// context.moveTo(25, 50); //horizontal, vertical
-	context.lineTo(400, 150);
-	context.lineCap = "butt";
-	context.stroke();
+// // 	// context.moveTo(25, 50); //horizontal, vertical
+// 	context.lineTo(400, 150);
+//  	context.lineCap = "butt";
+//  	context.stroke();
 
 
 	//draw second line change color etc
 
 	
-}
+//}
+
+
+
+
+// var c = document.getElementById("words");
+// var ctx = c.getContext("2d");
+// ctx.beginPath();
+// ctx.moveTo(0, 0);
+// ctx.lineTo(300, 150);
+// ctx.stroke();
+
